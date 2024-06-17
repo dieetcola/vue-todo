0cff916d05b2a457fe77f510fc2d2c694cdb22a9
Irakli Svanidze <svanidzeee@gmail.com>
2024-06-17 17:02:22 +0300

    used v-for dir to iterate over array list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,22 @@
 <script lang="ts" setup>
 import ListItem from "./ListItem.vue";
+
+type Item = {
+  title: string;
+  checked?: boolean;
+};
+
+const listItems: Item[] = [
+  { title: "Make a todo list app", checked: true },
+  { title: "Predict the weather", checked: false },
+  { title: "Play some tunes", checked: false },
+  { title: "Let's get cooking", checked: false },
+  { title: "Pump some iron", checked: false },
+  { title: "Track my expenses", checked: false },
+  { title: "Organize a game night", checked: false },
+  { title: "Learn a new language", checked: false },
+  { title: "Publish my work" },
+];
 </script>
 
 <template>
@@ -7,6 +24,17 @@ import ListItem from "./ListItem.vue";
     <!-- as i understand, if we want pass boolean to props, 
   must use this syntax, otherwise props should be a string
     -->
-    <ListItem :is-checked="false">This is the slotted content</ListItem>
+    <li v-for="(item, key) in listItems" :key="key">
+      <ListItem :is-checked="false">This is the slotted content</ListItem>
+    </li>
   </ul>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+li {
+  margin: 0.4rem 0;
+}
+</style>
