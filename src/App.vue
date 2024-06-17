ba21ce77c9d97289536e712a460d993183f30c5d
Irakli Svanidze <svanidzeee@gmail.com>
2024-06-17 15:27:32 +0300

    added header component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import AppHeader from "./components/AppHeader.vue";
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 1.2rem;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Every Vue application starts by creating a new application instance with the createApp function:
+createApp(App).mount('#app')
+// An application instance won't render anything until its .mount() method is called
