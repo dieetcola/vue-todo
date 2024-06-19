146368b737722f0c1b2ffd540b2620e4970ee9a5
Irakli Svanidze <svanidzeee@gmail.com>
2024-06-19 13:53:31 +0300

    store state to localstorage

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed, onMounted } from "vue";
 import type { Ref } from "vue";
 import ListItem from "./ListItem.vue";
 
@@ -8,41 +8,67 @@ type Item = {
   checked?: boolean;
 };
 
-const listItems: Ref<Item[]> = ref([
-  { title: "Make a todo list app", checked: true },
-  { title: "Predict the weather", checked: false },
-  { title: "Play some tunes", checked: false },
-  { title: "Let's get cooking", checked: false },
-  { title: "Pump some iron", checked: false },
-  { title: "Track my expenses", checked: false },
-  { title: "Organize a game night", checked: false },
-  { title: "Learn a new language", checked: false },
-  { title: "Publish my work" },
-]);
+const storageItems: Ref<Item[]> = ref([]);
+
+const setToStorage = (items: Item[]): void => {
+  localStorage.setItem("list-items", JSON.stringify(items));
+};
+
+const getFromStorage = (): Item[] | [] => {
+  const stored = localStorage.getItem("list-items");
+  if (stored) {
+    return JSON.parse(stored);
+  }
+  return [];
+};
+
+const initListItems = (): void => {
+  if (storageItems.value?.length === 0) {
+    const listItems: Item[] = [
+      { title: "Make a todo list app", checked: true },
+      { title: "Predict the weather", checked: false },
+      { title: "Read some comics", checked: false },
+      { title: "Let's get cooking", checked: false },
+      { title: "Pump some iron", checked: false },
+      { title: "Track my expenses", checked: false },
+      { title: "Organise a game night", checked: false },
+      { title: "Learn a new language", checked: false },
+      { title: "Publish my work", checked: false },
+    ];
+    setToStorage(listItems);
+    storageItems.value = listItems;
+  }
+};
+
+const sortedList = computed(() =>
+  [...storageItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
+);
 
 const updateItem = (item: Item): void => {
   const updatedItem = findItemInList(item);
   if (updatedItem) {
     toggleItemChecked(updatedItem);
+    setToStorage(storageItems.value);
   }
 };
 
 const findItemInList = (item: Item): Item | undefined => {
-  return listItems.value.find((itemInList: Item) => itemInList.title === item.title);
+  return storageItems.value.find((itemInList: Item) => itemInList.title === item.title);
 };
 
 const toggleItemChecked = (item: Item): void => {
   item.checked = !item.checked;
 };
-</script>
 
+onMounted(() => {
+  storageItems.value = getFromStorage();
+  initListItems();
+});
+</script>
 <template>
   <ul>
-    <!-- as i understand, if we want pass boolean to props, 
-  must use this syntax, otherwise props should be a string
-    -->
-    <li v-for="(item, key) in listItems" :key="key">
-      <ListItem :is-checked="item.checked" v-on:click. prevent="updateItem(item)">{{
+    <li :key="key" v-for="(item, key) in sortedList">
+      <ListItem :is-checked="item.checked" v-on:click.prevent="updateItem(item)">{{
         item.title
       }}</ListItem>
     </li>
@@ -53,6 +79,7 @@ const toggleItemChecked = (item: Item): void => {
 ul {
   list-style: none;
 }
+
 li {
   margin: 0.4rem 0;
 }
